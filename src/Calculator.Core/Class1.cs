03404489bfbc7aac362c3b3f53f13898605fd80d
XABIER PARADA RODRIGUEZ <T1007366@vidrala.com>
2025-08-25 11:14:08 +0200

    prueba fallando test unitario

--- src/Calculator.Core/Class1.cs
+++ src/Calculator.Core/Class1.cs
@@ -2,7 +2,7 @@
 
 public class CalculatorService
 {
-    public double Add(double a, double b) => a + b;
+    public double Add(double a, double b) => a - b;
     public double Subtract(double a, double b) => a - b;
     public double Multiply(double a, double b) => a * b;
     public double Divide(double a, double b)
